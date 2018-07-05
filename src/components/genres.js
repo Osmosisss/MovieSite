c717c753f18d686fcc32abc1dcad716d1c760c78
Osmosisss <ozyosmosis@gmail.com>
2018-07-05 11:37:05 -0600

    added pictures

--- src/components/genres.js
+++ src/components/genres.js
@@ -12,15 +12,6 @@ class Genres extends Component {
               </a>
               <p>
                 <b>Action</b>
-                <br />
-                <b>Powers:</b> super strength, flight, invulnerability, super
-                speed, heat vision, freeze breath, x-ray vision, superhuman
-                hearing, healing factor<br />
-                <b>Real Name:</b> Kal-El, Clark Joseph Kent
-                <br />
-                <b>Alias:</b> Man of Steel
-                <br />
-                <b>Weekness:</b> Kryptonite
               </p>
             </div>
             <div className="profile">
@@ -34,15 +25,6 @@ class Genres extends Component {
               </a>
               <p>
                 <b>Adventure</b>
-                <br />
-                <b>Powers:</b> super strength, durability, control over sea
-                life, exceptional swimming ability, ability to breathe
-                underwater<br />
-                <b>Real Name:</b> Arthur Curry
-                <br />
-                <b>Alias:</b> King of Atlantis
-                <br />
-                <b>Weekness:</b> Land
               </p>
             </div>
             <div className="profile">
@@ -51,64 +33,40 @@ class Genres extends Component {
               </a>
               <p>
                 <b>Comedy</b>
-                <br />
-                <b>Powers:</b> exceptional martial artist, combat strategy,
-                inexhaustible wealth, brilliant deductive skill, advanced
-                technology<br />
-                <b>Real Name:</b> Bruce Wayne
-                <br />
-                <b>Alias:</b> Dark Knight, Caped Crusader, Matches Malone
-                <br />
-                <b>Weekness:</b> Women, people that are close to him
               </p>
             </div>
             <div className="profile">
               <a href="https://www.dccomics.com/characters/wonder-woman">
-                <img src="assets/wonder.jpg" alt="wonder woman" />
+                <img
+                  src="assets/drama.jpeg"
+                  alt="wonder woman"
+                  width="200px"
+                  height="200px"
+                />
               </a>
               <p>
                 <b>Drama</b>
-                <br />
-                <b>Powers:</b> super strength, invulnerability, flight, combat
-                skill, combat strategy, superhuman agility, healing factor,
-                magic weaponry<br />
-                <b>Real Name:</b> Diana
-                <br />
-                <b>Alias:</b> Amazon Princess
-                <br />
-                <b>Weekness:</b> Guns, Poison
               </p>
             </div>
             <div className="profile">
               <a href="https://www.dccomics.com/characters/the-flash">
-                <img src="assets/flash.jpg" alt="the flash" />
+                <img src="assets/horror.jpg" alt="the flash" />
               </a>
               <p>
                 <b>Horror</b>
-                <br />
-                <b>Powers:</b> super speed, intangibility, superhuman agility<br />
-                <b>Real Name:</b> Barry Allen
-                <br />
-                <b>Alias:</b> Scarlet Speedster, The Fastest Man Alive
-                <br />
-                <b>Weekness:</b> Space/Time Manipulation, Cold Temperatures
               </p>
             </div>
             <div className="profile">
               <a href="https://www.dccomics.com/characters/cyborg">
-                <img src="assets/cyborg.jpg" alt="cyborg" width="350px" />
+                <img
+                  src="assets/suspense.jpeg"
+                  alt="cyborg"
+                  width="200px"
+                  height="200px"
+                />
               </a>
               <p>
                 <b>Suspense</b>
-                <br />
-                <b>Powers:</b> super strength, advanced technology, instant
-                weaponry, genius-level intellect, control over technology,
-                computer hacking, durability, teleportation<br />
-                <b>Real Name:</b> KVictor "Vic" Stone
-                <br />
-                <b>Alias:</b> Cyborg
-                <br />
-                <b>Weekness:</b> Capable of being shut down
               </p>
             </div>
           </div>
