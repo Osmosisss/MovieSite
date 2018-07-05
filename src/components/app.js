e3fb7ece93f42a4bc9c995bc6a883b38fce24be8
Osmosisss <ozyosmosis@gmail.com>
2018-07-05 13:24:31 -0600

    refactored code

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Header from "./header";
 import Genres from "./genres";
 
-import AnimateHeight from "react-animate-height";
-
 export default class App extends Component {
   render() {
     return (
--- src/components/genres.js
+++ src/components/genres.js
@@ -3,72 +3,26 @@ import React, { Component } from "react";
 class Genres extends Component {
   render() {
     return (
-      <div className="hero-page">
-        <div className="wrapper">
-          <div className="columns">
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/superman">
-                <img src="assets/action.jpg" alt="action" />
-              </a>
-              <p>
-                <b>Action</b>
-              </p>
-            </div>
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/aquaman">
-                <img
-                  src="assets/adventure.jpg"
-                  alt="adventure"
-                  width="200px"
-                  height="200px"
-                />
-              </a>
-              <p>
-                <b>Adventure</b>
-              </p>
-            </div>
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/batman">
-                <img src="assets/comedy.jpg" alt="batman" />
-              </a>
-              <p>
-                <b>Comedy</b>
-              </p>
-            </div>
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/wonder-woman">
-                <img
-                  src="assets/drama.jpeg"
-                  alt="wonder woman"
-                  width="200px"
-                  height="200px"
-                />
-              </a>
-              <p>
-                <b>Drama</b>
-              </p>
-            </div>
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/the-flash">
-                <img src="assets/horror.jpg" alt="the flash" />
-              </a>
-              <p>
-                <b>Horror</b>
-              </p>
-            </div>
-            <div className="profile">
-              <a href="https://www.dccomics.com/characters/cyborg">
-                <img
-                  src="assets/suspense.jpeg"
-                  alt="cyborg"
-                  width="200px"
-                  height="200px"
-                />
-              </a>
-              <p>
-                <b>Suspense</b>
-              </p>
-            </div>
+      <div className="main">
+        <div className="agg">
+          <div className="image">
+            <a href="#">
+              <img src="assets/action.jpg" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="agg">
+          <div className="image">
+            <a href="#">
+              <img src="assets/adventure.jpg" alt="adventure" />
+            </a>
+          </div>
+        </div>
+        <div className="agg">
+          <div className="image">
+            <a href="#">
+              <img src="assets/comedy.jpg" alt="" />
+            </a>
           </div>
         </div>
       </div>
